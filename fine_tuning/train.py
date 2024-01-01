c73f26d79d644832bbad82f0d2a91cf1baea3583
Yuri Kim <42161179+yrk3434@users.noreply.github.com>
2024-01-01 19:47:50 +0900

    Add files via upload

---
+++ fine_tuning/train.py
@@ -0,0 +1,7 @@
+import torch
+
+def collate_batch(batch):
+    data = [item[0] for item in batch]
+    mask = [item[1] for item in batch]
+    label = [item[2] for item in batch]
+    return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
---
+++ preprocess/clean_data.py
@@ -0,0 +1,97 @@
+import math
+import numpy as np
+import pandas as pd
+import random
+import re
+import torch
+import urllib.request
+from torch.utils.data import DataLoader, Dataset
+from transformers import PreTrainedTokenizerFast
+
+# 토크나이저에서 사용되는 스페셜 토큰들을 정의
+
+Q_TKN = "<usr>"
+A_TKN = "<sys>"
+BOS = '</s>'
+EOS = '</s>'
+MASK = '<unused0>'
+SENT = '<unused1>'
+PAD = '<pad>'
+
+# 허깅페이스 transformers 에 등록된 사전 학습된 koGTP2 토크나이저를 가져온다.
+koGPT2_TOKENIZER = PreTrainedTokenizerFast.from_pretrained("skt/kogpt2-base-v2", 
+                                                           bos_token=BOS, 
+                                                           eos_token=EOS, 
+                                                           unk_token="<unk>", # 모르는 단어를 나타내는 token
+                                                           pad_token=PAD, # 동일한 batch 내에서 입력의 크기를 동일하게 하기 위해서 사용해는 token
+                                                           mask_token=MASK)
+
+# 챗봇 데이터를 처리하는 클래스를 만든다.
+class ChatbotDataset(Dataset):
+    def __init__(self, chats, max_len=40):  # 데이터셋의 전처리를 해주는 부분
+        self._data = chats
+        self.max_len = max_len
+        self.q_token = Q_TKN
+        self.a_token = A_TKN
+        self.sent_token = SENT
+        self.eos = EOS
+        self.mask = MASK
+        self.tokenizer = koGPT2_TOKENIZER
+
+    def __len__(self):  # chatbotdata 의 길이를 리턴한다.
+        return len(self._data)
+
+    def __getitem__(self, idx):  # 로드한 챗봇 데이터를 차례차례 DataLoader로 넘겨주는 메서드
+        turn = self._data.iloc[idx]
+        q = turn["Q"]  # 질문을 가져온다.
+        q = re.sub(r"([?.!,])", r" ", q)  # 구둣점들을 제거한다.
+
+        a = turn["A"]  # 답변을 가져온다.
+        a = re.sub(r"([?.!,])", r" ", a)  # 구둣점들을 제거한다.
+
+        q_toked = self.tokenizer.tokenize(self.q_token + q + self.sent_token)
+        q_len = len(q_toked)
+
+        a_toked = self.tokenizer.tokenize(self.a_token + a + self.eos)
+        a_len = len(a_toked)
+
+        #질문의 길이가 최대길이보다 크면
+        if q_len > self.max_len:
+            a_len = self.max_len - q_len        #답변의 길이를 최대길이 - 질문길이
+            if a_len <= 0:       #질문의 길이가 너무 길어 질문만으로 최대 길이를 초과 한다면
+                q_toked = q_toked[-(int(self.max_len / 2)) :]   #질문길이를 최대길이의 반으로 
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len              #답변의 길이를 최대길이 - 질문길이
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+
+        #질문의 길이 + 답변의 길이가 최대길이보다 크면
+        if q_len + a_len > self.max_len:
+            a_len = self.max_len - q_len        #답변의 길이를 최대길이 - 질문길이
+            if a_len <= 0:       #질문의 길이가 너무 길어 질문만으로 최대 길이를 초과 한다면
+                q_toked = q_toked[-(int(self.max_len / 2)) :]   #질문길이를 최대길이의 반으로 
+                q_len = len(q_toked)
+                a_len = self.max_len - q_len              #답변의 길이를 최대길이 - 질문길이
+            a_toked = a_toked[:a_len]
+            a_len = len(a_toked)
+
+        # 답변 labels = [mask, mask, ...., mask, ..., <bos>,..답변.. <eos>, <pad>....]
+        labels = [self.mask,] * q_len + a_toked[1:]
+
+        # mask = 질문길이 0 + 답변길이 1 + 나머지 0
+        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len)
+        # 답변 labels을 index 로 만든다.
+        labels_ids = self.tokenizer.convert_tokens_to_ids(labels)
+        # 최대길이만큼 PADDING
+        while len(labels_ids) < self.max_len:
+            labels_ids += [self.tokenizer.pad_token_id]
+
+        # 질문 + 답변을 index 로 만든다.    
+        token_ids = self.tokenizer.convert_tokens_to_ids(q_toked + a_toked)
+        # 최대길이만큼 PADDING
+        while len(token_ids) < self.max_len:
+            token_ids += [self.tokenizer.pad_token_id]
+
+        #질문+답변, 마스크, 답변
+        return (token_ids, np.array(mask), labels_ids)
+
